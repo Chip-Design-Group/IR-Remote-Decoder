db20bafde9428c55120e973376e829cfc6ef2215
Katalin Szentmiklosy <katalin.szentmiklosy@hm.edu>
2026-02-16 10:26:54 +0000

    Reworked folder structure

--- IRRecoder_Replay/test/README.md
+++
@@ -1,7 +0,0 @@
-# test
-Cocotb-Tests fuer Unit- und Integrationsfaelle.
-
-Empfohlene Dateien:
-- `test_ir_recorder.py`
-- `test_ir_replay.py`
-- `test_ir_closed_loop.py`
---
+++ IRRecorder_Replay/Types_PKG/test/test_ir_types_pkg.py
@@ -0,0 +1,16 @@
+"""
+Datei: test_ir_types_pkg.py
+Zweck:
+  Contract-Test fuer Typ-/Bitfeld-Definitionen von ir_types_pkg.
+Rein:
+  Keine DUT-Stimuli; Fokus auf Datenvertrag (Pack/Unpack/Bitpositionen).
+Raus:
+  Validierung der erwarteten Feldzuordnung als Team-Vertrag.
+"""
+
+import pytest
+
+
+def test_types_pkg_contract_placeholder():
+    """TODO: Konsistenz von address/command/flags-Mapping pruefen."""
+    raise NotImplementedError("TODO: Test implementieren")
